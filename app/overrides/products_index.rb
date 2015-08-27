518f987004a44772003f3bdfe67541d26d1cd4c6
Hiroyuki Kondo <hiroyuki@chishaku.com>
2015-08-27 10:44:47 +0900

    set up as template

--- app/overrides/products_index.rb
+++ app/overrides/products_index.rb
@@ -1,6 +1,6 @@
-# -*- coding: utf-8 -*-
-Deface::Override.new(:virtual_path => "spree/shared/_products",
-                     :name => "translate_recently_viewed_products",
-                     :replace => "#recently_viewed h3.product-section-title",
-                     :text => '<h3 class="product-section-title">最近見た商品</h3>'
-                     )
+# # -*- coding: utf-8 -*-
+# Deface::Override.new(:virtual_path => "spree/shared/_products",
+#                      :name => "translate_recently_viewed_products",
+#                      :replace => "#recently_viewed h3.product-section-title",
+#                      :text => '<h3 class="product-section-title">最近見た商品</h3>'
+#                      )
--- app/overrides/products_show.rb
+++ app/overrides/products_show.rb
@@ -1,30 +1,30 @@
-# -*- coding: utf-8 -*-
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "translate_recently_viewed_products",
-                     :replace => "#recently_viewed h3.product-section-title",
-                     :text => '<h3 class="product-section-title">最近見た商品</h3>'
-                     )
+# # -*- coding: utf-8 -*-
+# Deface::Override.new(:virtual_path => "spree/products/show",
+#                      :name => "translate_recently_viewed_products",
+#                      :replace => "#recently_viewed h3.product-section-title",
+#                      :text => '<h3 class="product-section-title">最近見た商品</h3>'
+#                      )
 
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "translate_review",
-                     :replace => "#reviews h3.product-section-title",
-                     :text => '<h3 class="product-section-title">レビュー</h3>'
-                     )
+# Deface::Override.new(:virtual_path => "spree/products/show",
+#                      :name => "translate_review",
+#                      :replace => "#reviews h3.product-section-title",
+#                      :text => '<h3 class="product-section-title">レビュー</h3>'
+#                      )
+
+# # Deface::Override.new(:virtual_path => "spree/products/show",
+# #                      :name => "translate_write_your_own_review",
+# #                      :replace => "#reviews",
+# #                      :text => '<erb loud>link_to "レビューを書きませんか？", new_product_review_path(@product), class: "btn btn-default"</erb>'
+# #                      )
 
 # Deface::Override.new(:virtual_path => "spree/products/show",
 #                      :name => "translate_write_your_own_review",
 #                      :replace => "#reviews",
-#                      :text => '<erb loud>link_to "レビューを書きませんか？", new_product_review_path(@product), class: "btn btn-default"</erb>'
+#                      :text => ''
 #                      )
 
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "translate_write_your_own_review",
-                     :replace => "#reviews",
-                     :text => ''
-                     )
-
-Deface::Override.new(:virtual_path => "spree/products/show",
-                     :name => "translate_bookmark",
-                     :replace => "#social_nav h6",
-                     :text => '<h6>この商品を共有しませんか？</h6>'
-                     )
+# Deface::Override.new(:virtual_path => "spree/products/show",
+#                      :name => "translate_bookmark",
+#                      :replace => "#social_nav h6",
+#                      :text => '<h6>この商品を共有しませんか？</h6>'
+#                      )
